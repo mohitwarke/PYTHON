2f7772b5573fdd6aca2ea8d7d32ff785fddb9b6f
Mohit <70429369+mohitwarke@users.noreply.github.com>
2023-04-03 15:34:02 +0530

    Update python2.py

--- python2.py
+++ python2.py
@@ -9,4 +9,4 @@ files = sorted(files, key=lambda f: os.path.getmtime(os.path.join(folder_path, f
 latest_files = files[:5]
 
 for file in files[5:]:
-    os.remove(os.path.join(folder_path, file)).
+    os.remove(os.path.join(folder_path, file))
